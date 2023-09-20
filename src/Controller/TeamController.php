d1a5a34c28c7f5f8a917a42218e3a9385904aec9
Yoann Chocteau <yoann@chocteau.dev>
2023-09-20 17:22:26 +0200

    fix

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -16,26 +16,6 @@ class TeamController extends AbstractController
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): JsonResponse
     {
-        return $this->json('[
-    {
-        "uuid": "c43de0f1-b270-4f21-99c6-67ff608ce6be",
-        "name": "The winners",
-        "users": [
-            {
-                "uuid": "59aa9168-7ee4-4867-8e07-531f17139a37",
-                "name": "John Doe",
-                "email": "john@doe.local",
-                "city": "New York"
-            },
-            {
-                "uuid": "fcde031c-6551-4f35-a17b-6e721e6c0e3e",
-                "name": "Jane Doe",
-                "email": "jane@doe.local",
-                "city": "New York"
-            }
-        ]
-    }
-]');
-        //return $this->json($this->dataProvider->getTeamsWithUsers());
+        return $this->json($this->dataProvider->getTeamsWithUsers());
     }
 }
